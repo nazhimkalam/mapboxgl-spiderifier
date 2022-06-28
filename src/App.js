5a06c712875bc2d7baac121b3ea5714227c0207b
nazhimkalam <nazhimkalam@gmail.com>
2022-06-28 10:30:23 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
+import { clusterLayer, clusterCountLayer, unclusteredPointLayer, selectedCoordinatesLayer } from "./components/layers";
 import React, { useState, useEffect, useRef } from "react";
-import MapGL, { Marker, Map, Source, Layer } from "react-map-gl";
+import MapGL, { Marker, Source, Layer } from "react-map-gl";
 import { Editor, DrawPolygonMode } from "react-map-gl-draw";
 import inside from "point-in-polygon";
 import { ImLocation } from "react-icons/im";
-import { clusterLayer, clusterCountLayer, unclusteredPointLayer, selectedCoordinatesLayer } from "./components/layers";
 import "./style.css";
-
 import _ from "lodash";
 
-const MAPBOX_TOKEN =
-  "pk.eyJ1Ijoia2Vpbm8iLCJhIjoiOE5oc094SSJ9.DHxjhFy2Ef33iP8yqIm5cA";
+const MAPBOX_TOKEN = "pk.eyJ1Ijoia2Vpbm8iLCJhIjoiOE5oc094SSJ9.DHxjhFy2Ef33iP8yqIm5cA";
 
 const DEFAULT_VIEWPORT = {
   width: 800,
@@ -86,12 +84,11 @@ const geojson = {
 };
 
 export default function App() {
+  const [spiderifierMarkers, setSpiderifierMarkers] = useState([]);
   const [viewport, setViewport] = useState(DEFAULT_VIEWPORT);
-  const [markers, setMarkers] = useState({});
-  const [spiderifierMarkers, setSpiderifierMarkers] = useState([])
-  const [mode, setMode] = useState(null);
-  // const [isDataLoading, setIsDataLoading] = useState(true);
   const [features, setFeatures] = useState([]);
+  const [markers, setMarkers] = useState({});
+  const [mode, setMode] = useState(null);
   const mapRef = useRef(null);
 
   const updateViewport = (viewport) => {
@@ -101,42 +98,27 @@ export default function App() {
   useEffect(() => {
     if (geojson.features) {
       setMarkers(geojson);
-      // setIsDataLoading(false);
     }
   }, [geojson]);
 
-  useEffect(() => {
-    console.log("updated markers coordinates", markers);
-  }, [markers]);
-
   const handleUpdate = (val) => {
-    console.log('handle update data', val);
     setFeatures(val.data);
 
     if (val.editType === "addFeature") {
       const polygon = val.data[0].geometry.coordinates[0];
-      console.log('polygon', polygon);
       const newMarkers = markers.features.map((marker, i) => {
         const { geometry } = marker;
         const { coordinates } = geometry;
+        
         let longitude = coordinates[0];
         let latitude = coordinates[1];
 
-        console.log("longitude", longitude);
-        console.log("latitude", latitude);
         const isInsidePolygon = inside([longitude, latitude], polygon);
-        console.log("isInsidePolygon", isInsidePolygon);
 
-        return {
-          ...marker,
-          properties: { ...marker.properties, toggle: isInsidePolygon },
-        };
+        return { ...marker, properties: { ...marker.properties, toggle: isInsidePolygon } };
       });
 
       const updatedMarker = { ...markers, features: newMarkers };
-
-      console.log("newMarkers", newMarkers);
-      console.log("updatedMarker", updatedMarker);
       setMarkers(updatedMarker);
       setFeatures([]);
       setMode(null);
@@ -147,16 +129,13 @@ export default function App() {
     setMode(new DrawPolygonMode());
   };
 
-  const onClick = event => {
-    console.log("event features", event.features[0])
+  const onClick = (event) => {
     const isClusterArea = event.features[0].properties.cluster;
 
-    if (isClusterArea) { 
-      console.log("Yes it is a cluster area...")
-      const cluster_id = event.features[0].properties.cluster_id;
+    if (isClusterArea) {
       const number_of_coordinates = event.features[0].properties.point_count;
       const cluster_coordinates = event.features[0].geometry.coordinates;
-     
+
       var target_lat = cluster_coordinates[0];
       var target_lng = cluster_coordinates[1];
 
@@ -164,24 +143,28 @@ export default function App() {
       var min_difference_lng = 999;
       var closest_marker_coordinates = [];
 
-      markers.features.forEach((marker) => { 
+      markers.features.forEach((marker) => {
         const { geometry } = marker;
         const { coordinates } = geometry;
+        
         const marker_lat = coordinates[0];
         const lat_calc_difference = Math.abs(marker_lat - target_lat);
 
-        if (lat_calc_difference < min_difference_lat || min_difference_lat === 0) { 
+        if (lat_calc_difference < min_difference_lat || min_difference_lat === 0) {
           min_difference_lat = lat_calc_difference;
-          var closestMarker = findClosestLng(marker_lat, min_difference_lng, target_lng).geometry;
+          
+          var closestMarker = findClosestLng( marker_lat, min_difference_lng, target_lng).geometry;
           var closestMarkerCoordinates = closestMarker.coordinates;
+
           closest_marker_coordinates = [closestMarkerCoordinates[0], closestMarkerCoordinates[1]];
         }
       });
 
       var count = 0;
-      markers.features.forEach((marker) => { 
+      markers.features.forEach((marker) => {
         const { geometry } = marker;
         const { coordinates } = geometry;
+
         const marker_lat = coordinates[0];
         const marker_lng = coordinates[1];
         if (marker_lat === closest_marker_coordinates[0] && marker_lng === closest_marker_coordinates[1]) {
@@ -192,86 +175,52 @@ export default function App() {
       if (count === number_of_coordinates) {
         var listOfMarkers = createNewMarkers(closest_marker_coordinates, number_of_coordinates);
         setSpiderifierMarkers(listOfMarkers);
-        console.log("listOfMarkers", listOfMarkers);
       } else {
-        console.log('coordinate count mismatch!')
         setSpiderifierMarkers([]);
       }
     } else {
       setSpiderifierMarkers([]);
     }
-    
   };
 
   const createNewMarkers = (closest_marker_coordinates, number_of_coordinates) => {
-    var spiderifier_markers = []; 
+    var spiderifier_markers = [];
 
-    // for (var index = 1; index < number_of_coordinates+1; index++) {
-    //   let increment_lat = closest_marker_coordinates[1] + (index * 0.1);
-    //   let increment_lng = closest_marker_coordinates[0] + (index * 0.1);
+    for (var index = 1; index < number_of_coordinates + 1; index++) {
+      let spiral_lat = closest_marker_coordinates[1] + index * 1 * Math.sin(index * 0.25);
+      let spiral_lng = closest_marker_coordinates[0] + index * 2 * Math.cos(index * 0.2);
 
-    //   let marker = <Marker longitude={increment_lng} latitude={increment_lat} anchor="bottom" >
-    //                 <ImLocation style={{ color: 'red', fontSize: "2rem" }}/>
-    //               </Marker>;
-    //   spiderifier_markers.push(marker);
-    // }
-
-    for (var index = 1; index < number_of_coordinates+1; index++) { 
-      console.log("closest_marker_coordinates-creation", closest_marker_coordinates)
-      let spiral_lat = closest_marker_coordinates[1] + (index * 1 * Math.sin(index * 0.25));
-      let spiral_lng = closest_marker_coordinates[0] + (index * 2 * Math.cos(index * 0.2));
-
-      let marker = <Marker longitude={spiral_lng} latitude={spiral_lat} anchor="bottom" >
-                    <ImLocation style={{ color: 'red', fontSize: "2rem" }}/>
-                  </Marker>;
+      let marker = (
+        <Marker longitude={spiral_lng} latitude={spiral_lat} anchor="bottom">
+          <ImLocation style={{ color: "red", fontSize: "2rem" }} />
+        </Marker>
+      );
 
       spiderifier_markers.push(marker);
     }
 
     return spiderifier_markers;
-  }
-
-  const generateSpiralMarkerCoordinates = () => {
-    // generate spiral coordinates around the given center point [lat, lng] which is [14,20] for now
-    var spiral_coordinates = [];
-
-    var spiral_radius = 0.1;
-    var spiral_angle = 0;
-    var spiral_angle_increment = 0.1;
-    var spiral_angle_limit = 10;
-
-    while (spiral_angle < spiral_angle_limit) {
-      var spiral_x = spiral_radius * Math.cos(spiral_angle);
-      var spiral_y = spiral_radius * Math.sin(spiral_angle);
-
-      var spiral_coordinate = [spiral_x, spiral_y];
-      spiral_coordinates.push(spiral_coordinate);
-
-      spiral_angle += spiral_angle_increment;
-    }
-
-    return spiral_coordinates;
   };
 
   const findClosestLng = (targetLat, min_difference_lng, target_lng) => {
-    const markersWithTargetLat = markers.features.filter(marker => marker.geometry.coordinates[0] === targetLat);
+    const markersWithTargetLat = markers.features.filter((marker) => marker.geometry.coordinates[0] === targetLat);
     var closestMarker = undefined;
 
-    markersWithTargetLat.forEach((marker) => { 
+    markersWithTargetLat.forEach((marker) => {
       const { geometry } = marker;
       const { coordinates } = geometry;
 
       const marker_lng = coordinates[1];
       const lng_calc_difference = Math.abs(marker_lng - target_lng);
 
-      if (lng_calc_difference < min_difference_lng || min_difference_lng === 0) { 
+      if (lng_calc_difference < min_difference_lng || min_difference_lng === 0) {
         min_difference_lng = lng_calc_difference;
         closestMarker = marker;
       }
     });
 
     return closestMarker;
-  }
+  };
 
   return (
     <MapGL
@@ -284,37 +233,19 @@ export default function App() {
       onClick={onClick}
       ref={mapRef}
     >
-      <Editor
-        clickRadius={12}
-        mode={mode}
-        onUpdate={handleUpdate}
-        features={features}
-      />
+      <Editor clickRadius={12} mode={mode} onUpdate={handleUpdate} features={features} />
 
-      <Source
-        id="earthquakes"
-        type="geojson"
-        data={markers}
-        cluster={true}
-        clusterMaxZoom={14}
-        clusterRadius={50}
-      >
+      <Source id="earthquakes" type="geojson" data={markers} cluster={true} clusterMaxZoom={14} clusterRadius={50} >
         <Layer {...clusterLayer} />
         <Layer {...clusterCountLayer} />
         <Layer {...unclusteredPointLayer} />
         <Layer {...selectedCoordinatesLayer} />
       </Source>
 
-      {/* <Marker longitude={23.3} latitude={21.3} anchor="bottom" >
-        <ImLocation style={{ color: 'red', fontSize: "2rem" }}/>
-      </Marker> */}
-
       {spiderifierMarkers}
+
       <div className="toolbar-wrapper">
-        <div
-          className={`tool-wrapper ${mode ? "active" : ""}`}
-          onClick={handleModeChange}
-        >
+        <div className={`tool-wrapper ${mode ? "active" : ""}`} onClick={handleModeChange}>
           XX
         </div>
       </div>
